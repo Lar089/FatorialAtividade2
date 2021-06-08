5ab76bf0b0ebb9262993257cdb9b610dd1740783
Lar089 <carolinelara35@gmail.com>
2021-06-08 16:54:20 -0300

    Add project files.

---
+++ FatorialAtividade2/FatorialValores/Fatorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FatorialAtividade2.FatorialValores
+{
+    class Fatorial
+    {
+        public string str_final = "Saída: ";
+        private int valor_final;
+
+        public Fatorial(int valor)
+        {
+            this.str_final += GerarFatorial(valor);
+        }
+
+        private int GerarFatorial(int valor)
+        {
+            if (valor < 1)
+                valor_final = 1;
+
+            if (valor > 0)
+                valor_final = valor * GerarFatorial(valor - 1);
+
+            return valor_final;
+        }
+    }
+}
---
+++ FatorialAtividade2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using FatorialAtividade2.FatorialValores;
+
+namespace FatorialAtividade2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Fatorial(5).str_final);
+        }
+    }
+}
